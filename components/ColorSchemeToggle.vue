3a08e5b0e8b80a69a0ca36a7eca714c501a19a35
Hypnos <joashmacer@gmail.com>
2024-01-01 13:56:00 +0300

    piffle paffle

--- components/ColorSchemeToggle.vue
+++ components/ColorSchemeToggle.vue
@@ -8,6 +8,6 @@ function toggleMode() {
 
 <template>
   <a class="select-none" title="Toggle Color Scheme" @click="toggleMode">
-    <div dark:i-ph-sun-dim-duotone i-material-symbols-dark-mode-outline text-2xl />
+    <div i-ph-sun-dim-duotone dark:i-material-symbols-dark-mode-outline text-2xl />
   </a>
 </template>
