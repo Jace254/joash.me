973412389a13249db5864d139d833a7cce7c164d
Hypnos <joashmacer@gmail.com>
2024-01-01 05:15:04 +0300

    feat: github link

--- components/TheNav.vue
+++ components/TheNav.vue
@@ -1,9 +1,12 @@
 <template>
   <header class="blurred" md:w="calc(100% + var(--body-margin-left) + var(--body-margin-right))" w-full>
-    <nav mx-auto px4 lg:px-8 md:max-w-7xl flex items-center justify-between h-full z-20>
+    <nav mx-auto px4 lg:px-8 md:max-w-7xl flex items-center justify-between gap-2 h-full z-20>
       <Logo h-2em w-2em />
       <div flex-auto />
       <ColorSchemeToggle />
+      <a class="select-none" title="Github" href="https://github.com/Jace254" rel="noopener" target="_blank">
+        <div  i-carbon-logo-github/>
+      </a>
     </nav>
   </header>
 </template>
