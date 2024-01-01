34a8cdc1d81252914f71ff605a79960e341aa35e
Hypnos <joashmacer@gmail.com>
2024-01-01 05:24:07 +0300

    feat: twitter link

--- components/TheNav.vue
+++ components/TheNav.vue
@@ -1,11 +1,14 @@
 <template>
   <header class="blurred" md:w="calc(100% + var(--body-margin-left) + var(--body-margin-right))" w-full>
-    <nav mx-auto px4 lg:px-8 md:max-w-7xl flex items-center justify-between gap-2 h-full z-20>
+    <nav mx-auto px4 lg:px-8 md:max-w-7xl flex items-center justify-between gap-3 h-full z-20>
       <Logo h-2em w-2em />
       <div flex-auto />
+      <a class="select-none" title="Twitter" href="https://twitter.com/JoashMacenton" rel="noopener" target="_blank">
+        <div  i-carbon-logo-twitter text-1.5em/>
+      </a>
       <ColorSchemeToggle />
       <a class="select-none" title="Github" href="https://github.com/Jace254" rel="noopener" target="_blank">
-        <div  i-carbon-logo-github/>
+        <div  i-carbon-logo-github text-1.5em/>
       </a>
     </nav>
   </header>
