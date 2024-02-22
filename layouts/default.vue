895093edbc0267527d3e93bf154ff585b49921ad
Hypnos <47209391+Jace254@users.noreply.github.com>
2024-02-22 18:12:05 +0300

    chore: clean up pages

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,47 @@
 <script setup lang='ts'>
-import { SpeedInsights } from "@vercel/speed-insights/nuxt"
+import { SpeedInsights } from '@vercel/speed-insights/nuxt'
+import BeeetleBot from 'beeetle-chatbot-js'
+
+onMounted(() => {
+  BeeetleBot.initBubble({
+    apiHost: 'https://netwatch-api-concept.vercel.app/api/chat',
+    autoShowDelay: undefined,
+    brandColor: undefined,
+    isPreview: undefined,
+    beeetleBot: undefined,
+    onAnswer: undefined,
+    onClose: undefined,
+    onEnd: undefined,
+    onInit: undefined,
+    onNewInputBlock: undefined,
+    onOpen: undefined,
+    onPreviewMessageClick: undefined,
+    prefilledVariables: undefined,
+    previewMessage: undefined,
+    resultId: undefined,
+    theme: {
+      button: {
+        backgroundColor: undefined,
+        customCloseIconSrc: undefined,
+        customIconSrc: undefined,
+        iconColor: undefined,
+        size: undefined,
+      },
+      chatWindow: {
+        backgroundColor: undefined,
+        maxHeight: undefined,
+        maxWidth: undefined,
+      },
+      placement: undefined,
+      previewMessage: {
+        backgroundColor: undefined,
+        closeButtonBackgroundColor: undefined,
+        closeButtonIconColor: undefined,
+        textColor: undefined,
+      },
+    },
+  })
+})
 
 useCustomHead('Joash\'s Personal Webiste')
 </script>
--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { projects } from '@/lib'
-
 </script>
 
 <template>
