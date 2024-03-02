90597a857c2b306491e71eb7fceb8f9b129d018a
Hypnos <47209391+Jace254@users.noreply.github.com>
2024-03-02 23:49:34 +0300

    fix formatting

---
+++ pages/blog/[...slug].vue
@@ -0,0 +1,9 @@
+<template>
+  <article
+    w-full h-full max-w="var(--content-width)"
+    class="slide-enter-content prose"
+    md:px-0 px-7 relative block
+  >
+    <ContentDoc />
+  </article>
+</template>
