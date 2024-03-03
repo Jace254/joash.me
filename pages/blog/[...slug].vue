db9e583c89c16e5a73bb1a9241189269b2453af6
Hypnos <47209391+Jace254@users.noreply.github.com>
2024-03-03 19:28:29 +0300

    wrap post

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,9 +1,9 @@
+<script setup lang="ts">
+const { page } = useContent()
+</script>
+
 <template>
-  <article
-    w-full h-full max-w="var(--content-width)"
-    class="slide-enter-content prose"
-    md:px-0 px-7 relative block
-  >
+  <PostWrapper :frontmatter="page">
     <ContentDoc />
-  </article>
+  </PostWrapper>
 </template>
