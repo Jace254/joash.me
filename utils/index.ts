4897f8ad3da04e7472c61f3c350404c365f73a08
Hypnos <47209391+Jace254@users.noreply.github.com>
2024-03-03 18:07:37 +0300

    chore: add post type and post utils

--- utils/index.ts
+++ utils/index.ts
@@ -1,5 +1,6 @@
 import type { Updater } from '@tanstack/vue-table'
 import { type ClassValue, clsx } from 'clsx'
+import dayjs from 'dayjs'
 import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
@@ -11,4 +12,15 @@ export function valueUpdater<T extends Updater<any>>(updaterOrValue: T, ref: Ref
     = typeof updaterOrValue === 'function'
       ? updaterOrValue(ref.value)
       : updaterOrValue
+}
+
+export function formatDate(d: string | Date, onlyDate = true) {
+  const date = dayjs(d)
+  if (onlyDate || date.year() === dayjs().year())
+    return date.format('MMM D')
+  return date.format('MMM D, YYYY')
+}
+
+export function capitalizeFirstLetter(string: string) {
+  return string.charAt(0).toUpperCase() + string.slice(1)
 }
---
+++ utils/types.ts
@@ -0,0 +1,14 @@
+export interface Post {
+  _path: string
+  title: string
+  place?: string
+  date: string
+  desc?: string
+  platform?: string
+  duration?: string
+  recording?: string
+  radio?: boolean
+  video?: boolean
+  inperson?: boolean
+  redirect?: string
+}
