f8849d5201201b0a95db8225097a67850d2039ec
Hypnos <47209391+Jace254@users.noreply.github.com>
2024-03-06 06:34:38 +0300

    chore: projects

--- lib/index.ts
+++ lib/index.ts
@@ -1,27 +1,32 @@
 export interface Project {
   name: string
   description: string
+  year: string
   link: string
 }
 export const projects: Project[] = [
   {
     name: '100',
-    description: '100 day challenge on design concepts in web development.',
+    description: '100 day challenge on design and development',
+    year: '2024',
     link: 'https://100.joash.me',
-  },
-  {
-    name: 'CodeGround',
-    description: 'An automated docs and coding playground to learn programming in the node environment.',
-    link: 'https://code.joash.me',
   },
   {
     name: 'Beeetle Chatbot',
     description: 'A chatbot assitant for myself',
+    year: '2024',
     link: 'https://chatbot.joash.me',
+  },
+  {
+    name: 'CodeGround',
+    description: 'Automated docs and coding playground',
+    year: '2023',
+    link: 'https://code.joash.me',
   },
   {
     name: 'SurNote',
     description: 'A text editor',
+    year: '2023',
     link: 'https://sur.joash.me',
   },
 ]
--- pages/projects.vue
+++ pages/projects.vue
@@ -1,3 +1,191 @@
+<script setup lang="ts">
+import crypticTyping from 'cryptic-typing'
+import { projects } from '@/lib'
+
+const newProjects = ref(projects.map((_p) => { return { name: ' ', description: ' ', year: ' ' } }))
+
+onBeforeMount(() => {
+  projects.forEach((p, i) => {
+    crypticTyping(p.name, (n) => {
+      newProjects.value[i].name = n
+    }, false)
+    crypticTyping(p.description, (n) => {
+      newProjects.value[i].description = n
+    }, false)
+    crypticTyping(p.year, (n) => {
+      newProjects.value[i].year = n
+    }, true, 90)
+  })
+})
+
+definePageMeta({
+  layout: undefined,
+})
+</script>
+
 <template>
-  Projects
+  <main class="wrapper sizing w-full ">
+    <div v-for="project, idx in newProjects" :key="idx" class="link-wrapper">
+      <NuxtLink class="link" :href="project.link">
+        <span
+          class="project-title"
+          style="--i: 0; --c1: 'x'; --c2: '$'; --c3: '≈'"
+        >{{ project.name }}</span>
+        <span class="project-desc">{{ project.description }}</span>
+        <div class="animated-line" :style="`--d: ${idx}`" />
+        <div class="project-date">
+          {{ project.year }}
+        </div>
+      </NuxtLink>
+    </div>
+  </main>
 </template>
+
+<style>
+.wrapper {
+  max-width: 600px;
+}
+@media (max-width: 600px) {
+  .wrapper {
+    padding-top: 40px;
+  }
+}
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  min-height: 100vh;
+  margin: 0px auto;
+  overflow: hidden;
+  padding-left: 24px;
+  padding-right: 24px;
+  padding-bottom: 120px;
+  transition: padding-top 1s ease-in-out;
+}
+
+.sizing {
+  box-sizing: border-box;
+}
+
+.link-wrapper {
+  margin-top: 32px;
+  display: flex;
+  flex-direction: column;
+  gap: 28px;
+}
+
+.link {
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  width: calc(100% + 32px);
+  position: relative;
+  overflow: hidden;
+  padding: 12px;
+  margin: -12px;
+  border-radius: 12px;
+  outline: 0;
+  color: inherit;
+  text-decoration: none;
+  cursor: pointer;
+}
+
+.link:hover {
+  background-color: hsl(0 0% 13.6%);
+}
+
+.link::selection {
+  background: #fff9a8;
+  color: black;
+}
+
+.project-title {
+  display: inline-block;
+  color: hsl(0 0% 93%);
+  font-weight: 500;
+  line-height: 28px;
+  font-size: 14px;
+  margin: 0;
+}
+
+.project-desc {
+  margin: 0;
+  margin-left: -8px;
+  display: inline-block;
+  color: hsl(0 0% 62.8%);
+  font-weight: 400;
+  line-height: 28px;
+  font-size: 14px;
+}
+
+@media (max-width: 720px) {
+  .project-desc {
+    display: none;
+  }
+}
+
+.animated-line {
+  flex: 1 1 0%;
+  position: relative;
+  box-sizing: border-box;
+}
+
+.animated-line::before {
+  content: "";
+  position: absolute;
+  bottom: -1px;
+  height: 1px;
+  width: 0px;
+  z-index: 3;
+  animation: 1s ease calc(var(--d) * 0.02s) 1 normal forwards running shine;
+}
+
+.animated-line::after {
+  content: "";
+  position: absolute;
+  bottom: -1px;
+  height: 1px;
+  width: 0px;
+  animation: 1s ease calc(var(--d) * 0.02s) 1 normal forwards running settle;
+}
+
+.project-date {
+  color: hsl(0 0% 43.9%);
+  font-weight: 400;
+  display: block;
+  margin: 0;
+  line-height: 28px;
+  font-size: 14px;
+  font-family: X, -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
+    "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
+    "Helvetica Neue", sans-serif;
+}
+
+@keyframes shine {
+  0% {
+    background: transparent;
+  }
+
+  50% {
+    background: hsl(0 0% 49.4%);
+  }
+  100% {
+    width: 100%;
+    background: transparent;
+  }
+}
+
+@keyframes settle {
+  0% {
+    background: transparent;
+  }
+
+  50% {
+    background: hsl(0 0% 49.4%);
+  }
+  100% {
+    width: 100%;
+    background: hsl(0 0% 17.9%);
+  }
+}
+</style>import { onBeforeMount } from 'vue';
