b6ee7b7e4aceee0123cf5ee418c63a0cf6e6abb9
Hypnos <47209391+Jace254@users.noreply.github.com>
2024-05-13 16:37:06 +0300

    chore: update

---
+++ pages/[...all].vue
@@ -0,0 +1,12 @@
+<template>
+  <NuxtLayout>
+    <article class="prose">
+      <h1>Apologies</h1>
+      <p>
+        You seem Lost, Let's get you back <NuxtLink to="/">
+          home
+        </NuxtLink>
+      </p>
+    </article>
+  </NuxtLayout>
+</template>
