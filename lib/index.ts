2c0a2eaaf278a1de8dce44e1b4015e43d95b462e
Hypnos <47209391+Jace254@users.noreply.github.com>
2025-12-19 05:31:11 +0300

    chore: Update project list and add new blog post on JavaScript variable declarations

--- lib/index.ts
+++ lib/index.ts
@@ -4,53 +4,43 @@ export interface Project {
   year: string
   link: string
 }
+
 export const projects: Project[] = [
   {
+    name: 'Reliable Medical',
+    description: 'Reliable Medical Equipement website',
+    year: '2025',
+    link: 'https://reliablemedical.co.ke',
+  },
+  {
+    name: 'AIRFibers',
+    description: 'ISP Business Management Platform',
+    year: '2025',
+    link: 'https://app.airfibers.com',
+  },
+  {
     name: 'Sandblx',
-    description: 'Node Graph Visual Editor(Developing)',
-    year: '2024',
+    description: 'Node Graph Visual Editor For CRM Workflows(Developing)',
+    year: '2025',
     link: 'https://sandblx.joash.me',
   },
   {
     name: 'Beeetle Chatbot',
-    description: 'A chatbot assitant for myself',
+    description: 'An AI chatbot assistant for my website',
     year: '2024',
     link: 'https://chatbot.joash.me',
   },
+  {
+    name: 'Hotspot UI',
+    description: 'Wezeshanet WISP Hotspot user portal',
+    year: '2024',
+    link: 'https://hotspot.joash.me',
+  },
+
   {
     name: '100',
     description: '100 day challenge on design and development',
     year: '2024',
     link: 'https://100.joash.me',
-  },
-  {
-    name: 'Hotspot UI',
-    description: 'Wezeshanet WISP Hostpot user portal',
-    year: '2024',
-    link: 'https://hotspot.joash.me',
-  },
-  {
-    name: 'vitesse-nuxt3-shadcn',
-    description: 'vitesse-nuxt3 template with shadcn',
-    year: '2024',
-    link: 'https://vitesse-nuxt3-shadcn.vercel.app',
-  },
-  {
-    name: 'CodeGround',
-    description: 'Automated docs and coding playground',
-    year: '2023',
-    link: 'https://code.joash.me',
-  },
-  {
-    name: 'SurNote',
-    description: 'A text editor',
-    year: '2023',
-    link: 'https://sur.joash.me',
-  },
-  {
-    name: 'ISP Landing Page',
-    description: 'Wezeshanet WISP landing page',
-    year: '2023',
-    link: 'https://isp.wezeshanet.com/',
   },
 ]
